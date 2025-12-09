28e497e3d69c92a8710791f957fc31e23fa62dfa
Helen <frontendledy@gmail.com>
2025-12-09 15:34:50 +0300

    correction

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import './style.css';
 import './assets/images/image_sofa.png';
 import './assets/images/image_desk.png';
+import $ from "jquery";
+
+
+// маска для поля ввода номера телефона
+const phoneInput = $("#tel");
+phoneInput.mask('+7 (999) 999-99-99'); // маска для ввода номера телефона
